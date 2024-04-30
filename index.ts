d35c82543b38ce150a20046edad4a0d983b2386d
Bruno Bizeray <bbizeray@ciandt.com>
2024-04-30 16:24:35 -0300

    feat: add error handler

--- index.ts
+++ index.ts
@@ -1,23 +1,25 @@
-import cors from "cors";
-import express from "express";
-import "./src/logger";
-import { loggerFactory } from "./src/logger/index";
-import routes from "./src/routes";
-import { SystemConfig } from "./src/systemConfig/SystemConfig";
+import cors from 'cors';
+import express from 'express';
+import './src/logger';
+import { loggerFactory } from './src/logger/index';
+import routes from './src/routes';
+import { SystemConfig } from './src/systemConfig/SystemConfig';
+import { errorHandler } from './src/controllers/errorHandler/ErrorHandler';
 
 const logger = loggerFactory(__filename);
 const systemConfig = new SystemConfig();
 
 systemConfig.start().then(() => {
-    const app = express();
+  const app = express();
+  app.use(errorHandler);
+  app.use(cors());
+  app.use(express.json());
 
-    app.use(cors())
-    app.use(express.json());
+  app.use(routes);
 
-    app.use(routes);
-
-    return app.listen(process.env.SERVER_PORT || 3000, () => {
-        logger.info(`Server is running on port ${process.env.SERVER_PORT || 3000}...`)
-    });
+  return app.listen(process.env.SERVER_PORT || 3000, () => {
+    logger.info(
+      `Server is running on port ${process.env.SERVER_PORT || 3000}...`
+    );
+  });
 });
-
