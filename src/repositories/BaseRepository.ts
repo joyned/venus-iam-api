08be946d4014ff125d749d11d60ef11d736c39d9
Bruno Bizeray <bbizeray@ciandt.com>
2024-05-03 10:35:19 -0300

    fix: ajust to log error when query is not valid.

--- src/repositories/BaseRepository.ts
+++ src/repositories/BaseRepository.ts
@@ -4,8 +4,13 @@ import { loggerFactory } from '../logger';
 const logger = loggerFactory('BaseRepository');
 
 const executeQuery = async (query: string, params: any[] = []) => {
-  logger.info(`Executing query: ${query} with params: ${params}`);
-  return await pool.query(query, params);
+  try {
+    logger.info(`Executing query: ${query} with params: ${params}`);
+    return await pool.query(query, params);
+  } catch (e) {
+    logger.error(`Error executing query: ${query} with params: ${params}`, e);
+    throw e;
+  }
 };
 
 const beginTransaction = async () => {
