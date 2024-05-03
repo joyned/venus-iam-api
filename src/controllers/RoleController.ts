2b06071ff0bb7eb7d851c49d36a04190e322fb82
Bruno Bizeray <bbizeray@ciandt.com>
2024-05-02 21:22:41 -0300

    fix: bug in creating role.

--- src/controllers/RoleController.ts
+++ src/controllers/RoleController.ts
@@ -1,51 +1,65 @@
-import { Router } from "express";
-import { v4 } from "uuid";
-import { Role } from "../entities/Role";
-import { RoleService } from "../services/RoleService";
-import { mapRolesToDTO, mapRoleToDTO } from "./mapper";
-import { authenticationMiddleware } from "../middleware/AuthenticationMiddleware";
+import { Router } from 'express';
+import { v4 } from 'uuid';
+import { Role } from '../entities/Role';
+import { RoleService } from '../services/RoleService';
+import { mapRolesToDTO, mapRoleToDTO } from './mapper';
+import { authenticationMiddleware } from '../middleware/AuthenticationMiddleware';
 
 const roleController = Router();
 const service = new RoleService();
 
-roleController.get('/', authenticationMiddleware(['SYSTEM_ADMIN', 'SYSTEM_VIEWER']), async (req, res) => {
+roleController.get(
+  '/',
+  authenticationMiddleware(['SYSTEM_ADMIN', 'SYSTEM_VIEWER']),
+  async (req, res) => {
     return res.json(mapRolesToDTO(await service.findAll()));
-});
+  }
+);
 
-roleController.get('/:roleId', authenticationMiddleware(['SYSTEM_ADMIN', 'SYSTEM_VIEWER']), async (req, res) => {
+roleController.get(
+  '/:roleId',
+  authenticationMiddleware(['SYSTEM_ADMIN', 'SYSTEM_VIEWER']),
+  async (req, res) => {
     return res.json(mapRoleToDTO(await service.findById(req.params.roleId)));
-});
+  }
+);
 
-roleController.post('/', authenticationMiddleware(['SYSTEM_ADMIN']), async (req, res) => {
+roleController.post(
+  '/',
+  authenticationMiddleware(['SYSTEM_ADMIN']),
+  async (req, res) => {
     let role: Role = new Role();
 
     if (req.body.id) {
-        role.id = req.body.id;
-    } else {
-        role.id = v4();
+      role.id = req.body.id;
     }
 
     role.name = req.body.name;
-    role.createdAt = new Date();
+    role.createdAt = req.body.createdAt;
 
     try {
-        return res.json(await service.persist(role));
+      return res.json(await service.persist(role));
     } catch (error) {
-        return res.status(500).json(error);
+      return res.status(500).json(error);
     }
-})
+  }
+);
 
-roleController.delete('/:roleId', authenticationMiddleware(['SYSTEM_ADMIN']), async (req, res) => {
+roleController.delete(
+  '/:roleId',
+  authenticationMiddleware(['SYSTEM_ADMIN']),
+  async (req, res) => {
     try {
-        const result = await service.delete(req.params.roleId);
-        if (result) {
-            return res.json(req.params.roleId);
-        } else {
-            return res.status(404).json({ message: 'Not found.' });
-        }
+      const result = await service.delete(req.params.roleId);
+      if (result) {
+        return res.json(req.params.roleId);
+      } else {
+        return res.status(404).json({ message: 'Not found.' });
+      }
     } catch (error: any) {
-        return res.status(500).json(error);
+      return res.status(500).json(error);
     }
-})
+  }
+);
 
 export default roleController;
--- src/repositories/RoleRepository.ts
+++ src/repositories/RoleRepository.ts
@@ -7,7 +7,7 @@ const FIND_ROLE_BY_GROUP_ID = `select r.* from venus."role" r join venus.group_r
 const FIND_ROLES_BY_USER_ID = `select r.* from venus."role" r join venus.group_role gr on gr.role_id = r.id join venus.user_group ug on ug.user_id = $1 and ug.group_id = gr.group_id`;
 const INSERT =
   'INSERT INTO venus."role" (id, "name", created_at) VALUES($1, $2, CURRENT_TIMESTAMP)';
-const UPDATE = 'UPDATE venus."role" SET "name"=$2, created_at=$3 WHERE id=$1';
+const UPDATE = 'UPDATE venus."role" SET "name"=$2 WHERE id=$1';
 const DELETE = `DELETE FROM venus."role" WHERE id=$1`;
 
 export class RoleRepository {
@@ -37,9 +37,10 @@ export class RoleRepository {
     let result = undefined;
     if (!role.id) {
       role.id = v4();
+      role.createdAt = new Date();
       result = await executeQuery(INSERT, [role.id, role.name]);
     } else {
-      result = await executeQuery(UPDATE, [role.id, role.name, role.createdAt]);
+      result = await executeQuery(UPDATE, [role.id, role.name]);
     }
 
     if (result.rowCount == 1) {
