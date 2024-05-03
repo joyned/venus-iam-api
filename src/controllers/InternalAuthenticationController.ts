8a0f6d59fbcc1119aaa7ad0a5cce5896286173eb
Bruno Bizeray <bbizeray@ciandt.com>
2024-05-02 21:16:36 -0300

    fix: add error handle on internal login

--- src/controllers/InternalAuthenticationController.ts
+++ src/controllers/InternalAuthenticationController.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { InvalidPasswordError } from '../exceptions/InvalidPasswordError';
 import { AuthenticationService } from '../services/AuthenticationService';
 
 const internalAuthenticationController = Router();
@@ -6,7 +7,14 @@ const service = new AuthenticationService();
 
 internalAuthenticationController.post('/login', async (req, res) => {
   const { email, password } = req.body;
-  return res.json(await service.login(email, password));
+  try {
+    return res.json(await service.login(email, password));
+  } catch (error) {
+    if (error instanceof InvalidPasswordError) {
+      return res.status(401).json({ message: error.message });
+    }
+    return res.status(500).json({ message: 'Internal server error.' });
+  }
 });
 
 export default internalAuthenticationController;
